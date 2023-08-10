fec2a2814658a3f885dcb7bd73fd49c2be823b8f
I2C\mmushtaq02 <mmushtaq02@i2cinc.com>
2023-08-10 18:41:30 +0500

    Deby Embedded Bb test data added!

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO expense_categories (expense_category_id, expense_category_title) VALUES (1, 'Breakfast');
+INSERT INTO expense_categories (expense_category_id, expense_category_title) VALUES (2, 'Lunch');
+INSERT INTO expense_categories (expense_category_id, expense_category_title) VALUES (3, 'Dinner');
+
+
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (1, 3, '2023-08-05-00.00.00', 25.00, 'Burger Hub');
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (2, 1, '2023-08-06-00.00.00', 250.00, 'Chacha Chekka');
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (3, 3, '2023-08-06-00.00.00', 25.00, 'My Burger');
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (4, 2, '2023-08-06-00.00.00', 25.00, 'Karachi Birani');
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (5, 3, '2023-08-10-00.00.00', 25.00, 'Jalal Sons');
+INSERT INTO expenses (expense_id, expense_category_id, expense_date, expense_amount, comments) VALUES (6, 2, '2023-08-10-00.00.00', 25.00, 'Butt Karahi');
