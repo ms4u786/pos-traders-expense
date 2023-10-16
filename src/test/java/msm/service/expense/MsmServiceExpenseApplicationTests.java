becb448d2d7b3372561fd205bc6f8cddc8fb8f9b
Muhammad Shahbaz Mushtaq <mshahbazmushtaq@gmail.com>
2023-10-16 23:06:21 +0500

    Service package name standardized.

--- src/test/java/msm/service/expense/MsmServiceExpenseApplicationTests.java
+++ src/test/java/msm/service/expense/MsmServiceExpenseApplicationTests.java
@@ -1,10 +1,9 @@
 package msm.service.expense;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
-class PosTradersExpenseApplicationTests {
+class MsmServiceExpenseApplicationTests {
 
     @Test
     void contextLoads() {
